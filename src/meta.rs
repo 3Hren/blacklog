9ce32f8163511d36dda6b987b9b4301c7d1a8278
Evgeny Safronov <division494@gmail.com>
2016-06-21 01:31:57 +0300

    feat: implement signed and unsidned conversions

--- src/meta.rs
+++ src/meta.rs
@@ -6,7 +6,7 @@ pub enum Value<'a> {
     Nil,
     Bool(bool),
     Signed(i64),
-    // Unsigned(u64),
+    Unsigned(u64),
     // Float(f64),
     String(Cow<'a, str>),
     // Func(&'a Fn(&mut Write) -> Result<(), ::std::io::Error>),
@@ -27,6 +27,54 @@ impl<'a> From<i8> for Value<'a> {
 impl<'a> From<i16> for Value<'a> {
     fn from(val: i16) -> Value<'a> {
         Value::Signed(val as i64)
+    }
+}
+
+impl<'a> From<i32> for Value<'a> {
+    fn from(val: i32) -> Value<'a> {
+        Value::Signed(val as i64)
+    }
+}
+
+impl<'a> From<i64> for Value<'a> {
+    fn from(val: i64) -> Value<'a> {
+        Value::Signed(val)
+    }
+}
+
+impl<'a> From<isize> for Value<'a> {
+    fn from(val: isize) -> Value<'a> {
+        Value::Signed(val as i64)
+    }
+}
+
+impl<'a> From<u8> for Value<'a> {
+    fn from(val: u8) -> Value<'a> {
+        Value::Unsigned(val as u64)
+    }
+}
+
+impl<'a> From<u16> for Value<'a> {
+    fn from(val: u16) -> Value<'a> {
+        Value::Unsigned(val as u64)
+    }
+}
+
+impl<'a> From<u32> for Value<'a> {
+    fn from(val: u32) -> Value<'a> {
+        Value::Unsigned(val as u64)
+    }
+}
+
+impl<'a> From<u64> for Value<'a> {
+    fn from(val: u64) -> Value<'a> {
+        Value::Unsigned(val)
+    }
+}
+
+impl<'a> From<usize> for Value<'a> {
+    fn from(val: usize) -> Value<'a> {
+        Value::Unsigned(val as u64)
     }
 }
 
@@ -110,6 +158,46 @@ mod tests {
     #[test]
     fn from_i16() {
         assert_eq!(Value::Signed(4200), From::from(4200i16));
+    }
+
+    #[test]
+    fn from_i32() {
+        assert_eq!(Value::Signed(42000), From::from(42000i32));
+    }
+
+    #[test]
+    fn from_i64() {
+        assert_eq!(Value::Signed(420000000), From::from(420000000i64));
+    }
+
+    #[test]
+    fn from_isize() {
+        assert_eq!(Value::Signed(4200), From::from(4200isize));
+    }
+
+    #[test]
+    fn from_u8() {
+        assert_eq!(Value::Unsigned(42), From::from(42u8));
+    }
+
+    #[test]
+    fn from_u16() {
+        assert_eq!(Value::Unsigned(4200), From::from(4200u16));
+    }
+
+    #[test]
+    fn from_u32() {
+        assert_eq!(Value::Unsigned(42000), From::from(42000u32));
+    }
+
+    #[test]
+    fn from_u64() {
+        assert_eq!(Value::Unsigned(420000000), From::from(420000000u64));
+    }
+
+    #[test]
+    fn from_usize() {
+        assert_eq!(Value::Unsigned(4200), From::from(4200usize));
     }
 
     #[test]
