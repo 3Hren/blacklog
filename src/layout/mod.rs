faaa311f0c93e5648d25ef1704fe029c1c5d0021
Evgeny Safronov <division494@gmail.com>
2016-06-30 11:06:17 +0300

    docs: some docs doesn't hurt anyone

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -18,6 +18,8 @@ quick_error! {
     }
 }
 
+/// Layouts are responsible for formatting a log event into a form that meets the needs of whatever
+/// will be consuming the log event.
 pub trait Layout: Send + Sync {
     fn format(&self, rec: &Record, wr: &mut Write) -> Result<(), Error>;
 }
