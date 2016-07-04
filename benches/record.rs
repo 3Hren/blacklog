fb221444de415f0a2c48a994deebc385893fc7d9
Evgeny Safronov <division494@gmail.com>
2016-07-04 11:01:14 +0300

    refactor: record benchmarking review

--- benches/record.rs
+++ benches/record.rs
@@ -1,48 +1,56 @@
-// #![feature(test)]
-//
-// extern crate blacklog;
-// extern crate test;
-//
-// use test::Bencher;
-//
-// use blacklog::{Meta, MetaList, Record};
-//
-// /// This benchmark demonstrates, that creating an inactive record is very cheap.
-// #[bench]
-// fn new(b: &mut Bencher) {
-//     b.iter(|| {
-//         Record::new(0, line!(), module_path!(), format_args!("le message"), &MetaList::new(&[]));
-//     });
-// }
-//
-// /// This benchmark demonstrates, that creating an inactive record is very cheap, even with comlex
-// /// formatting pattern and meta attributes.
-// #[bench]
-// fn new_with_format_and_meta6(b: &mut Bencher) {
-//     b.iter(|| {
-//         Record::new(0, line!(), module_path!(),
-//             format_args!("le message: {}, {}", 42, "maybe later"),
-//             &MetaList::new(&[Meta::new("meta#1", &42),
-//             Meta::new("meta#1", &42),
-//             Meta::new("meta#1", &42),
-//             Meta::new("meta#1", &42),
-//             Meta::new("meta#1", &42),
-//             Meta::new("meta#1", &42)]));
-//     });
-// }
-//
-// #[bench]
-// fn activate_with_format(b: &mut Bencher) {
-//     b.iter(|| {
-//         Record::new(0, line!(), module_path!(), format_args!("le {}", "message"), &MetaList::new(&[]))
-//             .activate();
-//     });
-// }
-//
-// #[bench]
-// fn activate_without_format(b: &mut Bencher) {
-//     b.iter(|| {
-//         Record::new(0, line!(), module_path!(), format_args!("le message"), &MetaList::new(&[]))
-//             .activate();
-//     });
-// }
+#![feature(test)]
+
+extern crate blacklog;
+extern crate chrono;
+extern crate test;
+
+use test::Bencher;
+
+use chrono::{DateTime, UTC};
+use chrono::naive::date::NaiveDate;
+use chrono::naive::datetime::NaiveDateTime;
+use chrono::naive::time::NaiveTime;
+
+use blacklog::{Meta, MetaList, Record};
+
+#[bench]
+fn chrono_from_timestamp(b: &mut Bencher) {
+    b.iter(|| {
+        let timestamp: DateTime<UTC> = DateTime::from_utc(NaiveDateTime::from_timestamp(0, 0), UTC);
+        test::black_box(timestamp);
+    });
+}
+
+#[bench]
+fn chrono_new(b: &mut Bencher) {
+    b.iter(|| {
+        let timestamp: DateTime<UTC> = DateTime::from_utc(
+            NaiveDateTime::new(NaiveDate::from_ymd(0000, 01, 01), NaiveTime::from_hms(0, 0, 0)),
+            UTC
+        );
+        test::black_box(timestamp);
+    });
+}
+
+/// This benchmark demonstrates, that creating an inactive record is very cheap.
+#[bench]
+fn new(b: &mut Bencher) {
+    b.iter(|| {
+        Record::new(0, line!(), module_path!(), &MetaList::new(&[]));
+    });
+}
+
+/// This benchmark demonstrates, that creating an inactive record is very cheap, even with meta
+/// attributes.
+#[bench]
+fn new_with_format_and_meta6(b: &mut Bencher) {
+    b.iter(|| {
+        Record::new(0, line!(), module_path!(),
+            &MetaList::new(&[Meta::new("meta#1", &42),
+            Meta::new("meta#1", &42),
+            Meta::new("meta#1", &42),
+            Meta::new("meta#1", &42),
+            Meta::new("meta#1", &42),
+            Meta::new("meta#1", &42)]));
+    });
+}
