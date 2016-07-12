a285b2848d6b948555ac7bf2b40967bfa35f6bd4
Evgeny Safronov <division494@gmail.com>
2016-07-12 13:17:20 +0300

    feat: update examples

--- examples/config.rs
+++ examples/config.rs
@@ -1,13 +1,45 @@
 //! This example demonstrates how to initialize the Blacklog from the JSON file and write some
 //! messages using it with runtime formatting.
+//! Also a custom severity binding is shown.
 
 extern crate serde_json;
 #[macro_use] extern crate blacklog;
 
 use std::env;
 use std::fs::File;
+use std::io::Error;
 
-use blacklog::Registry;
+use blacklog::{Format, Formatter, Severity, Registry};
+
+enum Level {
+    Debug,
+    Info,
+    Warn,
+    Error,
+}
+
+impl Severity for Level {
+    fn num(&self) -> i32 {
+        match *self {
+            Level::Debug => 0,
+            Level::Info => 1,
+            Level::Warn => 2,
+            Level::Error => 3,
+        }
+    }
+
+    fn format(val: i32, format: &mut Formatter) -> Result<(), Error>
+        where Self: Sized
+    {
+        match val {
+            0 => format.write_str("DEBUG"),
+            1 => format.write_str("INFO"),
+            2 => format.write_str("WARN"),
+            3 => format.write_str("ERROR"),
+            val => val.format(format),
+        }
+    }
+}
 
 fn main() {
     let path = env::args()
@@ -22,8 +54,8 @@ fn main() {
         .logger(&cfg)
         .expect("expect logger to be properly created");
 
-    log!(logger, 0, "{} {} HTTP/1.1 {} {}", "GET", "/static/image.png", 404, 347);
-    log!(logger, 1, "nginx/1.6 configured");
-    log!(logger, 2, "client stopped connection before send body completed");
-    log!(logger, 3, "file does not exist: {}", "/var/www/favicon.ico");
+    log!(logger, Level::Debug, "{} {} HTTP/1.1 {} {}", "GET", "/static/image.png", 404, 347);
+    log!(logger, Level::Info, "nginx/1.6 configured");
+    log!(logger, Level::Warn, "client stopped connection before send body completed");
+    log!(logger, Level::Error, "file does not exist: {}", "/var/www/favicon.ico");
 }
