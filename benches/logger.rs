3dfb57d392d6586e55e70b2b9d82f0986aae7398
Evgeny Safronov <division494@gmail.com>
2016-07-15 19:33:13 +0300

    refactor: move benchmarks out

--- benches/logger.rs
+++ benches/logger.rs
@@ -18,6 +18,22 @@ fn sync_log(b: &mut Bencher) {
 }
 
 #[bench]
+fn sync_log_with_format_and_meta6(b: &mut Bencher) {
+    let log = SyncLogger::new(vec![]);
+
+    b.iter(|| {
+        log!(log, 0, "file does not exist: {}", ["/var/www/favicon.ico"], {
+            flag: true,
+            path1: "/home1",
+            path2: "/home2",
+            path3: "/home3",
+            path4: "/home4",
+            path5: "/home5",
+        });
+    });
+}
+
+#[bench]
 fn actor_log(b: &mut Bencher) {
     let log = ActorLogger::new(vec![]);
 
@@ -27,12 +43,30 @@ fn actor_log(b: &mut Bencher) {
 }
 
 #[bench]
-fn fast_filtered_out_sync_log(b: &mut Bencher) {
+fn sync_log_with_sev_adapter_deny(b: &mut Bencher) {
     let log = SyncLogger::new(vec![]);
     let log = SeverityFilteredLoggerAdapter::new(log);
     log.filter(1);
 
     b.iter(|| {
         log!(log, 0, "file does not exist: /var/www/favicon.ico");
+    });
+}
+
+#[bench]
+fn sync_log_with_format_and_meta6_with_sev_adapter_deny(b: &mut Bencher) {
+    let log = SyncLogger::new(vec![]);
+    let log = SeverityFilteredLoggerAdapter::new(log);
+    log.filter(1);
+
+    b.iter(|| {
+        log!(log, 0, "file does not exist: {}", ["/var/www/favicon.ico"], {
+            flag: true,
+            path1: "/home1",
+            path2: "/home2",
+            path3: "/home3",
+            path4: "/home4",
+            path5: "/home5",
+        });
     });
 }
